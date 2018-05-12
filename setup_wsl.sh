89955ce649d32263f2c80810385a6ca84bddf5fe
Tyler Schwemley <tjschwem@gmail.com>
2018-05-12 12:12:53 -0400

    updated permisisons and added gitignore

--- setup_wsl.sh
+++ setup_wsl.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-mkdir /mnt/c/www /mnt/c/bin /mnt/c/bin/scripts
+mkdir /mnt/c/www /mnt/c/bin
 mkdir $USER/.config/terminator
+mv scripts /mnt/c/bin/
+mv terminator.lnk /mnt/c/bin/terminator.lnk
+mv vcxsrv.exe.lnk /mnt/c/bin/vcxsrv.lnk
+mv keymaps.ahk /mnt/c/Users/tjsch/AppData/Roaming/Microsoft/Windows/Start\ Menu/Programs/Startup/
 
 apt-get update
 apt-get install terminator
